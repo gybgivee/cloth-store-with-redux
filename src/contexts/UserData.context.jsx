4429e8d1963fddee1719bc1190ac5cdb98f763a8
gybgivee <kmcn0512@gmail.com>
2022-08-01 15:16:19 +0100

    basic-done

--- src/contexts/UserData.context.jsx
+++ src/contexts/UserData.context.jsx
@@ -1,11 +1,12 @@
 import { createContext, useState, useEffect } from "react";
 import { onAuthStateChangedListener,createUserDocumentFromAuth} from "../utilities/firebase/firebase.utils";
-export const UserContext = createContext(
+export const UserDataContext = createContext(
     {
         currentUser: null,
         setCurrentUser: () => null,
     }
 )
+
 //component
 export const UserProvider = ({ children }) => {
     const [currentUser, setCurrentUser] = useState(null);
@@ -19,11 +20,11 @@ export const UserProvider = ({ children }) => {
             }
             //if logout there will be no value in user => user=null
             setCurrentUser(user);
-            console.log('userContext:', user);
+            console.log('UserDataContext:', user);
         });
         //return it to remove event listeners when it done
         return unsubscribe;
     }, []);
     //unMounted
-    return <UserContext.Provider value={value}>{children}</UserContext.Provider>
+    return <UserDataContext.Provider value={value}>{children}</UserDataContext.Provider>
 }
